78bf171b5522165e6a630d3be9a3803b1fa29239
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-06 17:08:48 -0400

    Creación de método para mostrar el menú

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    
+
+    public static void mostrarMenu() {
+        System.out.println("\n=========================");
+        System.out.println("  REGISTRO DE USUARIOS");
+        System.out.println("=========================");
+        System.out.println("\n1. Registrar usuario");
+        System.out.println("2. Salir");
+    }
 }
